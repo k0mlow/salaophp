bfafcaf38507a9e48cf9dcf2f295de125420db79
Aluno <Aluno@AFI-261506>
2023-03-30 11:11:22 -0300

    projeto 23/03

--- index.php
+++ index.php
@@ -6,7 +6,28 @@
     </head>
     <body>
         <?php
-        // put your code here
+        require_once $_SERVER['DOCUMENT_ROOT'].'/salaophp/model/dao/ClienteDAO.php';
+        
+        $u = new Cliente();
+        $u -> setNome('duda');
+        $u ->setEmail('Eduarda');
+        $u -> setSenha('afogados');
+
+        $dao = ClienteDAO::getInstance();
+        $dao->insert($u);
+        echo "Salvou mais um usuário...";
+        
+        //teste do getById
+        $testeobj = ClienteDAO::getInstance()->getById(172665);
+        print_r($testeobj);
+        
+        //teste do atualizar
+        $testeobj->setEmail('eduarda@gmail.com');
+        ClienteDAO::getInstance()->update($testeobj);
+        
+        //teste do delete;
+        ClienteDAO::getInstance()-> delete(172667);
+        
         ?>
     </body>
 </html>
---
+++ model/dao/ClienteDAO.php
@@ -0,0 +1,97 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/salaophp/model/dao/BDPDO.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/salaophp/model/vo/Cliente.php';
+
+
+class ClienteDAO {
+    public static $instance;
+    private function __construct() {
+}
+public static function getInstance() {
+    if (!isset(self::$instance))
+        self::$instance = new ClienteDAO();
+             return self::$instance;
+    
+}
+   public function insert(Cliente $usuario){
+       try {
+           $sql = "INSERT INTO cliente (nome,email,senha) "
+                   . "VALUES (:nome,:email,:senha)";
+           //perceba que na linha abaixo vai precisar de um import
+           $p_sql = BDPDO::getInstance()->prepare($sql);
+           $p_sql->bindValue(":nome", $usuario->getNome());
+           $p_sql->bindValue(":email", $usuario->getEmail());
+           //iremos critografar a senha para md5, asism o usuário terá mais segurança, já que frequentemente usamos a mesma senha para diversas aplicações.
+           $p_sql->bindValue(":senha", md5($usuario->getSenha()));
+           return $p_sql->execute();
+       } catch (Exception $e) {
+           print "Erro ao executar a função de salvar".$e->getMessage();
+   
+       }
+
+}
+   public function update($usuario){
+        try {
+           $sql = "UPDATE cliente SET nome = :nome,"
+                   . "email = :email,"
+                   . "senha = :senha "
+                   . "where id=:id";
+                   
+           //perceba que na linha abaixo vai precisar de um import
+           $p_sql = BDPDO::getInstance()->prepare($sql);
+           $p_sql->bindValue(":nome", $usuario->getNome());
+           $p_sql->bindValue(":email", $usuario->getEmail());
+           //iremos critografar a senha para md5, asism o usuário terá mais segurança, já que frequentemente usamos a mesma senha para diversas aplicações.
+           $p_sql->bindValue(":senha", md5($usuario->getSenha()));
+           $p_sql->bindValue(":id", $usuario->getId());
+           return $p_sql->execute();
+       } catch (Exception $e) {
+           print "Erro ao executar a função de atualizar".$e->getMessage();
+   
+       }
+       
+   }
+   public function delete($id){
+       try {
+           $sql = "delete from cliente where id = :id";
+           //perceba que na linha abaixo vai precisar de um import
+           $p_sql = BDPDO::getInstance()->prepare($sql);
+           $p_sql->bindValue(":id", $id);
+           return $p_sql->execute();
+       } catch (Exception $e) {
+           print "Erro ao executar a função de deletar".$e->getMessage();
+   
+       }
+       
+   }
+   public function getById($id){
+       try {
+            $sql = "SELECT * FROM cliente WHERE id = :id";
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            $p_sql->bindValue(":id", $id);
+            $p_sql->execute();
+            return $this->converterLinhaDaBaseDeDadosParaObjeto($p_sql->fetch(PDO::FETCH_ASSOC));
+        } catch (Exception $e) {
+            print "Ocorreu um erro ao tentar executar esta ação, foi gerado
+ um LOG do mesmo, tente novamente mais tarde.";
+            GeraLog::getInstance()->inserirLog("Erro: Código: " . $e->
+getCode() . " Mensagem: " . $e->getMessage());
+        }
+   }
+   public function listAll(){}
+    
+   
+   
+   private function converterLinhaDaBaseDeDadosParaObjeto($row) {
+        $obj = new Cliente;
+        $obj->setId($row['id']);
+        $obj->setNome($row['nome']);
+        $obj->setEmail($row['email']);
+        $obj->setSenha($row['senha']);
+        return $obj;
+    }
+   
+}
+
+
+?>
